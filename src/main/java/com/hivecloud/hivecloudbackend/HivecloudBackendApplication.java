f133a6abc8a60478f05aec4207de7fa9b8fbda11
Augusto Silva <augusto_ls17@hotmail.com>
2019-11-27 02:09:17 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/hivecloud/hivecloudbackend/HivecloudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hivecloud.hivecloudbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HivecloudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HivecloudBackendApplication.class, args);
+	}
+
+}
